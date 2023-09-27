816c43b7342e8a5114db0a60c5587e5e05997f01
LiruOvO <115586635+LiruOvO@users.noreply.github.com>
2023-09-27 14:59:24 +0300

    Додаткове завдання

---
+++ Додаткове дз/Additional Task/Additional Task/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Additional_Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Exit = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.YesBut = new System.Windows.Forms.Button();
+            this.NoBut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Exit
+            // 
+            this.Exit.AutoSize = true;
+            this.Exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(227)))), ((int)(((byte)(228)))));
+            this.Exit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Exit.Font = new System.Drawing.Font("Verdana", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Exit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.Exit.Location = new System.Drawing.Point(365, 9);
+            this.Exit.Margin = new System.Windows.Forms.Padding(0);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(26, 24);
+            this.Exit.TabIndex = 2;
+            this.Exit.Text = "X";
+            this.Exit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            this.Exit.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Exit_MouseDown);
+            this.Exit.MouseEnter += new System.EventHandler(this.Exit_MouseEnter);
+            this.Exit.MouseLeave += new System.EventHandler(this.Exit_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(199)))), ((int)(((byte)(200)))));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 122);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Вас задовольнає ваша стипендія?\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // YesBut
+            // 
+            this.YesBut.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.YesBut.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.YesBut.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.YesBut.FlatAppearance.BorderSize = 3;
+            this.YesBut.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.YesBut.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.YesBut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.YesBut.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold);
+            this.YesBut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(199)))), ((int)(((byte)(200)))));
+            this.YesBut.Location = new System.Drawing.Point(127, 181);
+            this.YesBut.Name = "YesBut";
+            this.YesBut.Size = new System.Drawing.Size(140, 44);
+            this.YesBut.TabIndex = 4;
+            this.YesBut.Text = "Так";
+            this.YesBut.UseVisualStyleBackColor = false;
+            this.YesBut.Click += new System.EventHandler(this.YesBut_Click);
+            // 
+            // NoBut
+            // 
+            this.NoBut.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.NoBut.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.NoBut.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.NoBut.FlatAppearance.BorderSize = 3;
+            this.NoBut.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.NoBut.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(57)))), ((int)(((byte)(57)))));
+            this.NoBut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NoBut.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold);
+            this.NoBut.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(199)))), ((int)(((byte)(200)))));
+            this.NoBut.Location = new System.Drawing.Point(127, 266);
+            this.NoBut.Name = "NoBut";
+            this.NoBut.Size = new System.Drawing.Size(140, 44);
+            this.NoBut.TabIndex = 5;
+            this.NoBut.Text = "Ні";
+            this.NoBut.UseVisualStyleBackColor = false;
+            this.NoBut.Click += new System.EventHandler(this.NoBut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(199)))), ((int)(((byte)(200)))));
+            this.ClientSize = new System.Drawing.Size(400, 550);
+            this.Controls.Add(this.NoBut);
+            this.Controls.Add(this.YesBut);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button YesBut;
+        private System.Windows.Forms.Button NoBut;
+    }
+}
+
---
+++ Додаткове дз/Additional Task/Additional Task/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Additional_Task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Exit_MouseEnter(object sender, EventArgs e)
+        {
+            Exit.ForeColor = Color.FromArgb(68, 113, 114);
+            Exit.BackColor = Color.FromArgb(135, 227, 228);
+        }
+
+        private void Exit_MouseLeave(object sender, EventArgs e)
+        {
+            Exit.ForeColor = Color.FromArgb(34, 57, 57);
+        }
+
+        private void Exit_MouseDown(object sender, MouseEventArgs e)
+        {
+            Exit.BackColor = Color.FromArgb(101, 170, 171);
+            Exit.ForeColor = Color.FromArgb(34, 57, 57);
+        }
+
+        private void NoBut_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int newX = random.Next(0, 260);
+            int newY = random.Next(122, 506);
+            NoBut.Location = new Point(newX, newY);
+        }
+
+        private void YesBut_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Звичайно, як може не подобатись аж 1980 грн на місяць за виконання мільйон дз на день))))))))");
+        }
+    }    
+}
